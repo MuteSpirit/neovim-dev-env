0d633914b413060fa5527b409bab58d73aad56f1
Ivan Efimov <mutespirit@yandex.ru>
2026-05-19 16:34:24 +0500

    fix(neovim-dev-env.sh): add mounting config folders fot ssh, tmux and nvim
    Mount also:
    * $HOME/.tmux
    * $HOME/.ssh
    * $HOME/.local/state/nvim

--- neovim-dev-env.sh
+++ neovim-dev-env.sh
@@ -4,9 +4,12 @@ docker run --rm -ti \
     --env ENV_USER="$USER" \
     --env ENV_USER_ID="$(id -u)" \
     --env ENV_USER_GROUP_ID="$(id -g)" \
-    --volume "$HOME/.gitconfig":"$HOME/.gitconfig" \
-    --volume "$HOME/.config/nvim":"$HOME/.config/nvim" \
-    --volume "$HOME/.local/share/nvim":"$HOME/.local/share/nvim" \
+    --volume "$HOME/.gitconfig":"$HOME/.gitconfig":ro \
+    --volume "$HOME/.tmux":"$HOME/.tmux":ro \
+    --volume "$HOME/.ssh":"$HOME/.ssh":ro \
+    --volume "$HOME/.config/nvim":"$HOME/.config/nvim":rw \
+    --volume "$HOME/.local/share/nvim":"$HOME/.local/share/nvim":rw \
+    --volume "$HOME/.local/state/nvim":"$HOME/.local/state/nvim":rw \
     --volume "$(pwd):/ws" \
     --workdir "/ws" \
     neovim-dev-env "cd /ws; tmux"
